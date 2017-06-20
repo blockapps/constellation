3d33434a506ef4440b49718bda3f864a624cfe49
Martin Allen <martin.allen26@gmail.com>
2017-06-20 02:02:50 -0400

    full proxy of constellation

--- Constellation/Node/ServantApi.hs
+++ Constellation/Node/ServantApi.hs
@@ -1,23 +1,49 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE StrictData #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Constellation.Node.ServantApi where
 
+
+import Control.Monad.IO.Class
+import Control.Error
 import Data.Aeson
     (FromJSON(parseJSON), ToJSON(toJSON), Value(Object), (.:), (.=), object)
-import Data.ByteString
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as C8
+import Control.Monad.Error.Class
 import Data.Text (Text)
+import Data.Proxy
 import Data.ByteArray.Encoding (Base(Base64), convertToBase, convertFromBase)
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Network.HTTP.ReverseProxy
+import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
+
+
+import Servant.Server.Internal.ServantErr
 import qualified Data.Text.Encoding as TE
-import qualified Network.Wai as Wai
+
+import qualified Network.HTTP.Types as H
 
 import Constellation.Enclave.Types (PublicKey)
 import Constellation.Util.ByteString (mustB64TextDecodeBs, b64BsDecodeText)
 
 import Servant
+import Servant.Client
+
+import System.Environment (lookupEnv)
+import System.IO.Unsafe (unsafePerformIO)
+
+import           Network.Wai.Handler.Warp (run)
+
+--------------------------------------------------------------------------------
+-- * API Types
+--------------------------------------------------------------------------------
 
 data SendRequest = SendRequest
     { sreqPayload :: ByteString
@@ -94,10 +120,109 @@ instance FromJSON DeleteRequest where
 instance ToJSON DeleteRequest where
   toJSON delReq = object [ "key" .= dreqKey delReq ]
 
-data DeleteResponse = DeleteResponse deriving (Show)
+--------------------------------------------------------------------------------
+-- | API
+--------------------------------------------------------------------------------
 
 type API = "send" :> ReqBody '[JSON] SendRequest :> Post '[JSON] SendResponse
       :<|> "sendRaw" :> Raw
       :<|> "receive" :> ReqBody '[JSON] ReceiveRequest :> Post '[JSON] ReceiveResponse
       :<|> "receiveRaw" :> ReqBody '[JSON] ReceiveRequest :> Post '[JSON] ReceiveResponse
-      :<|> "delete" :> ReqBody '[JSON] DeleteRequest :> Post '[JSON] DeleteResponse
+      :<|> "delete" :> ReqBody '[JSON] DeleteRequest :> Post '[JSON] NoContent
+
+--------------------------------------------------------------------------------
+-- * Client
+--------------------------------------------------------------------------------
+
+postSendClient :: SendRequest -> ClientM SendResponse
+postReceiveClient :: ReceiveRequest -> ClientM ReceiveResponse
+postReceiveRawClient :: ReceiveRequest -> ClientM ReceiveResponse
+postDeleteClient :: DeleteRequest -> ClientM NoContent
+
+postSendClient
+  :<|> _
+  :<|> postReceiveClient
+  :<|> postReceiveRawClient
+  :<|> postDeleteClient = client (Proxy @API)
+
+--------------------------------------------------------------------------------
+-- * Server
+--------------------------------------------------------------------------------
+
+server :: Server API
+server = postSend
+  :<|> postSendRaw
+  :<|> postReceive
+  :<|> postReceive
+  :<|> postDelete
+
+
+postSend :: SendRequest -> Handler SendResponse
+postSend sReq = do
+  resp <- liftIO $ runClientM (postSendClient sReq) constellationEnv
+  either (throwError . clientErrToServantErr) return resp
+
+postSendRaw :: Application
+postSendRaw =
+  let (ClientEnv mgr (BaseUrl _ hst prt _)) = constellationEnv
+      dest = ProxyDest (C8.pack hst) prt
+  in waiProxyTo (const . return $ WPRProxyDest dest) defaultOnExc mgr
+
+postReceive :: ReceiveRequest -> Handler ReceiveResponse
+postReceive rReq = do
+  resp <- liftIO $ runClientM (postReceiveClient rReq) constellationEnv
+  either (throwError . clientErrToServantErr) return resp
+
+postReceiveRaw :: ReceiveRequest -> Handler ReceiveResponse
+postReceiveRaw rReq = do
+  resp <- liftIO $ runClientM (postReceiveRawClient rReq) constellationEnv
+  either (throwError . clientErrToServantErr) return resp
+
+postDelete :: DeleteRequest -> Handler NoContent
+postDelete dReq = do
+  resp <- liftIO $ runClientM (postDeleteClient dReq) constellationEnv
+  either (throwError . clientErrToServantErr) return resp
+
+--------------------------------------------------------------------------------
+-- ** Application
+--------------------------------------------------------------------------------
+
+app :: Application
+app = logStdoutDev $ serve (Proxy @API) server
+
+constellationEnv :: ClientEnv
+constellationEnv = unsafePerformIO $ do
+  mgr <- newManager tlsManagerSettings
+  ebaseUrl <- makeBaseUrl
+  case ebaseUrl of
+    Left e -> error e
+    Right baseUrl -> return $ ClientEnv mgr baseUrl
+{-# NOINLINE constellationEnv #-}
+
+makeBaseUrl :: IO (Either String BaseUrl)
+makeBaseUrl = runExceptT $ do
+    sch <- lookupEnv "CONSTELLATION_SCHEME" !? "Missing Env Var: CONSTELLATION_SCHEME" >>= parseScheme
+    host <- lookupEnv "CONSTELLATION_HOST" !? "Missing Env Var: CONSTELLATION_HOST"
+    port <- lookupEnv "CONSTELLATION_PORT" !? "Missing Env Var: CONSTELLATION_PORT" >>= return . read
+    path <- lookupEnv "CONSTELLATION_PATH" !? "Missing Env Var: CONSTELLATION_PATH"
+    return $ BaseUrl sch host port path
+  where
+    parseScheme :: MonadError String m => String -> m Scheme
+    parseScheme sch = case sch of
+      "Http" -> return Http
+      "Https" -> return Https
+      _ -> throwError "URI scheme must be Http or Https."
+
+initProxyApp :: IO ()
+initProxyApp = do
+  mprt <- lookupEnv "PROXY_PORT"
+  maybe (error "Missing Env Var: PROXY_PORT") (flip run app . read) mprt
+
+--------------------------------------------------------------------------------
+clientErrToServantErr :: ServantError -> ServantErr
+clientErrToServantErr sErr = case sErr of
+  FailureResponse{..} -> ServantErr (H.statusCode responseStatus) "" responseBody []
+  DecodeFailure{..} -> ServantErr 422 "" responseBody []
+  UnsupportedContentType{..} -> ServantErr 422 "" responseBody []
+  InvalidContentTypeHeader{..} -> ServantErr 415 "" responseBody []
+  ConnectionError{..} -> err500 {errBody = "Connection Error" }
